09a0045f92c2f664bbc87ad0864cd3e7b81f38d9
Andrew <Andrew@LAPTOP-JFM53C5G>
2016-12-29 01:27:04 -0800

    All new added stuff - 12/28/16

--- Win32Project1/Win32Project1/Hero.h
+++ Win32Project1/Win32Project1/Hero.h
@@ -1,20 +1,27 @@
+#ifndef HERO_H
+#define HERO_H
+
 #include <iostream>
 #include <SFML\Graphics.hpp>
+#include "Foreign.h"
 
 class Hero {
-	sf::RectangleShape hero; //hitbox 
+	sf::RectangleShape hitbox; //hitbox 
 	sf::Texture heroTexture;
 	sf::Sprite heroSprite;
+	const float cooldown = 200000;
+	float delay = 0;
+	int health = 100;
 public:
-	Hero(sf::Vector2f size, sf::Vector2f newPos, const sf::Texture& newTexture) {
-		hero.setSize(size);
-		hero.setPosition(newPos);
-		hero.setFillColor(sf::Color::Red);
-
+	Hero(sf::Vector2f newPos, const sf::Texture& newTexture) {
 		heroSprite.setPosition(newPos);
 		heroTexture = newTexture;
 		heroSprite.setTexture(heroTexture);
 		heroSprite.setTextureRect(sf::IntRect(0, 0, 64, 64));
+
+		hitbox.setSize({ 30, 59 });
+		hitbox.setPosition(newPos.x + 17, newPos.y + 5);
+		hitbox.setFillColor(sf::Color::Transparent);
 	}
 	void setTextureRect(sf::IntRect &newRect) {
 		heroSprite.setTextureRect(newRect);
@@ -22,11 +29,43 @@ public:
 
 	void draw(sf::RenderWindow &window) {
 		window.draw(heroSprite);
-		window.draw(hero);
+		window.draw(hitbox);
 	}
 
 	void move(sf::Vector2f distance) {
-		heroSprite.move(distance);
+		if (health > 0) {
+			heroSprite.move(distance);
+			hitbox.move(distance);
+		}
+	}
+
+	void update(float time) {
+		std::cout << delay << std::endl;
+		delay -= time;
+	}
+
+	sf::FloatRect getGlobalBounds() {
+		return hitbox.getGlobalBounds();
+	}
+
+	void showHitBox() {
+		hitbox.setFillColor(sf::Color::Blue);
+	}
+
+	bool foreignInteract(Foreign &object) {
+		if (delay <= 0) {
+			if (hitbox.getGlobalBounds().intersects(object.getGlobalBounds())) {
+				delay = cooldown;
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+
+	void takeDamage(const int x) {
+		health -= x;
+		std::cout << "Health: " << health << std::endl;
 	}
 
 	int getY() {
@@ -40,5 +79,6 @@ public:
 	void setY(float position) {
 		heroSprite.setPosition({ heroSprite.getPosition().x, position });
 	}
-
 };
+
+#endif
